ee7170456be919f2d79167bd8103fb6668735c76
KarlStarkweather <stark13@usa.net>
2022-02-06 11:03:02 -0800

    add code for Round class

---
+++ RockPaperScissors/Models/Round.cs
@@ -0,0 +1,18 @@
+namespace RockPaperScissors.Models
+{
+  public class Round
+  {
+    public int Player1Move {get; set;}
+    public int Player2Move {get; set;}
+
+    public string DeclareWinner()
+    {
+
+    }
+
+    public Round(int player1Move, int player2Move)
+    {
+
+    }
+  }
+}
