01732f66dd5c6d422a32deb9877c6f6e40a6f7a6
KarlStarkweather <stark13@usa.net>
2022-02-06 14:26:53 -0800

    initial Program file with UI to give instructions and input player 1 move

---
+++ RockPaperScissors/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using RockPaperScissors.Models;
+
+namespace RockPaperScissors
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Welcome to Rock, Paper, Scissors!");
+      Console.WriteLine("Enter either 'rock', 'paper' or 'scissors' as the move for Player 1:");
+      string P1 = Console.ReadLine();
+
+      // Round newRound = new Round(P1,P2);
+
+    }
+  }
+}
