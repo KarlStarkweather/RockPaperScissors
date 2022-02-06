ffb5eaa41bcb12e90096a5ed98062ce87cd0042c
KarlStarkweather <stark13@usa.net>
2022-02-06 14:30:00 -0800

    complete basic UI to input moves and declare winner

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -10,8 +10,10 @@ namespace RockPaperScissors
       Console.WriteLine("Welcome to Rock, Paper, Scissors!");
       Console.WriteLine("Enter either 'rock', 'paper' or 'scissors' as the move for Player 1:");
       string P1 = Console.ReadLine();
-
-      // Round newRound = new Round(P1,P2);
+      Console.WriteLine("Enter either 'rock', 'paper' or 'scissors' as the move for Player 2:");
+      string P2 = Console.ReadLine();
+      Round newRound = new Round(P1,P2);
+      Console.WriteLine(newRound.DeclareWinner());
 
     }
   }
